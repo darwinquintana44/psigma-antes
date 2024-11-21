88ae5f4becfe4c4c78e6807f131bde01585fe8ef
darwin quintana <darwin.q@psigmacorp.com>
2024-11-20 20:00:43 -0500

    se cambia la ruta local por la del servidor

--- src/environments/environments.ts
+++ src/environments/environments.ts
@@ -1,6 +1,6 @@
 export const environments = {
   production: false,
   defaultLanguage: 'es',
-  // baseUrl: 'https://antes-back.psigma.co',
-  baseUrl: 'http://127.0.0.1:8000',
+  baseUrl: 'https://antes-back.psigma.co',
+  // baseUrl: 'http://127.0.0.1:8000',
 }
